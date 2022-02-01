e6ebd40eb48e158c659c7f061afcb40272624d8c
blake214 <blakekellett10@gmail.com>
2022-02-01 14:56:56 +0200

    I have implemented session tracking

--- End_year/Our_project/index.js
+++ End_year/Our_project/index.js
@@ -1,23 +1,71 @@
+//////////////////////// Development stuff to remove
+// Here we just storing come credentials to test with
+global.users = [
+  {id:1, username:'John', password:'pass'},
+  {id:2, username:'Harry', password:'pass'}
+]
+//////////////////////// Development stuff to remove
+
+// This is for parsing data in the body of post requests
+const bodyParser = require("body-parser");
+// This our express server library
 const express = require("express");
+// This our sessions library for the loggin in and out
+const session = require("express-session");
+// This the library for our database
 const sqlite3 = require('sqlite3').verbose();
+
+// Here we creating our server
 const app = express();
 const port = 8082;
+
+//////////////////////// Database
+// Here we referencing our databse
 const db = new sqlite3.Database('./db/easy-catalogue.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) console.error(err.message);
     else console.log('Connected to the database.');
   });
+// Here we make our database global so main.js can see it
 global.db = db;
+//////////////////////// Database
 
+//////////////////////// Sessions
+// Configure our sessions
+const session_lifetime = 1000 * 60 * 60 // This is one hour session
+// Here we making the session id global so main.js can see it
+global.session_name = "session_id"
+const session_secret = "ssh!quiet,it\'asecrete!" // this is a random string to sign the cookie
+app.use(session({
+  name: session_name,
+  resave: false, // this we dont create a session each time there is a request if the session hasnt changed
+  saveUninitialized: false, // this is not to save a session with no data
+  secret: session_secret,
+  cookie: {
+    maxAge: session_lifetime, // this saying how long session is active
+    sameSite: true, // this saying only one client can be accessing the session
+    secure: false // this is true or false, false is fine when working on the production
+  }
+}))
+//////////////////////// Sessions
+
+//////////////////////// Body parser
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
+//////////////////////// Body parser
+
+//////////////////////// Server
 app.listen(port, () => console.log(`Easy Catalogue is listening on port ${port}!`));
-
 require("./routes/main")(app);
-
 app.set("views", __dirname + "/views");
-
 app.set("view engine", "ejs");
-
 app.engine("html", require("ejs").renderFile);
+//////////////////////// Server
 
 // To run the server:
 // 1) Start the server in the terminal - node index.js
+// 2) Go to browser and enter the url 'localhost:port' where port will be the port number
+
+// To run server with nodemon
+// 1) Start the server in the terminal - npm run dev
 // 2) Go to browser and enter the url 'localhost:port' where port will be the port number
--- End_year/Our_project/routes/main.js
+++ End_year/Our_project/routes/main.js
@@ -1,9 +1,35 @@
 module.exports = function (app) {
+    ////////////////////// Middleware functions
+    /** Here we redirect user back to login page
+     * If the user has logged in and has a session id
+     */
+    const redirectLogin = (req,res,next) => {
+        if(!req.session.userId){
+            res.redirect('/login')
+        } else next();
+    }
+    /** Here we redirect user back to user page
+     * If the user has logged in and has a session id
+     */
+    const redirectUser = (req,res,next) => {
+        if(req.session.userId){
+            res.redirect('/user')
+        } else next();
+    }
+    ////////////////////// Middleware functions
+
+    ////////////////////// Get Requests
     app.get("/", function (req, res) {
-        res.render("index.html")
+        // Here we getting the session id (will be undefind if not logged in)
+        const userId = req.session.userId;
+        // Here we parsing the session id to the template page
+        res.render("index.html", {userSessionId: userId});
     });
-    app.get("/user", function (req, res) {
-        res.render("user.html")
+    app.get("/user", redirectLogin, function (req, res) {
+        // Here we getting the username thats with the session id
+        const user = users.find(user => user.id === req.session.userId)
+        // Here we parsing the username to the template
+        res.render("user.html", {username: user.username})
     });
     app.get("/catalogue", function (req, res) {
         res.render("catalogue.html")
@@ -11,4 +37,70 @@ module.exports = function (app) {
     app.get("/product", function (req, res) {
         res.render("product.html")
     });
+    // In pipeline we calling redirectUser (basically checks if user is loged in or not)
+    app.get("/login", redirectUser, function (req, res) {
+        res.render("login.html")
+    });
+    // In pipeline we calling redirectUser (basically checks if user is loged in or not)
+    app.get("/register", redirectUser, function (req, res) {
+        res.render("register.html")
+    });
+    ////////////////////// Get Requests
+
+    ////////////////////// Post Requests
+    app.post("/login", redirectUser, function (req, res) {
+        // Here we getting the data from the submitted form
+        const username = req.body.username
+        const password = req.body.password
+        // Here we checking that stuff has been submitted
+        if (username && password){
+            /**Here we searching our array of credentials
+             * So if there is a username and password match, user gets populated
+             * else will be undefined
+             */
+            const user = users.find(
+                user => user.username === username && user.password === password
+            )
+            // If user is populated we create our session id and redirect to the user page
+            if (user){
+                req.session.userId = user.id
+                return res.redirect('/user');
+            }
+        }
+        // else back to login page
+        return res.redirect('/login')
+    });
+    app.post("/register", redirectUser, function (req, res) {
+        const username = req.body.username
+        const password = req.body.password
+        if (username && password){
+            // Here we checking if there is a username that already exists as what user put in
+            const exists = users.some(user => user.username === username)
+            if (!exists){
+                // Here we creating an object to push to our credentials array
+                const user = {
+                    id:users.length+1,
+                    username,
+                    password
+                }
+                users.push(user)
+                // Here we create our session id and redirect user to the user page
+                req.session.userId = user.id
+                return res.redirect('/user')
+            } 
+        }
+        // else back to register page
+        return res.redirect('/register')
+    });
+    app.post("/logout", redirectLogin, function (req, res) {
+        // This is a function that terminates the session
+        req.session.destroy(err=>{
+            // If there is a problem terminating the session then they just need to retry
+            if(err) return res.redirect('/user');
+            res.clearCookie(session_name)
+            // Redirects back to the login page
+            res.redirect('/login')
+        })
+    });
+    ////////////////////// Post Requests
 }
