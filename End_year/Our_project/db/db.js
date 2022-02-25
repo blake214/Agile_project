44c31fe3400a802917f88943e5fffaa9ca7ec6e6
Hadar Sharon <hadars@crazylabs.com>
2022-02-25 20:05:49 +0200

    get product fields dynamically

--- End_year/Our_project/db/db.js
+++ End_year/Our_project/db/db.js
@@ -1,28 +1,28 @@
 const api = require('./../api/api')
 const sqlite3 = require('sqlite3').verbose()
+const DBPath = './easy-catalogue.db'
 
 function insertProductToDB (brand, productCode) {
-  const db = new sqlite3.Database('./easy-catalogue.db')
-  api.getProduct(brand, productCode).then((productObject) => {
-    const productFields = [
-      'product_code',
-      'product_name',
-      'product_description_short',
-      'product_description_long',
-      'product_img_urls',
-      'product_category',
-      'product_brand',
-      'product_specs'
-    ]
-    const data = productFields.map(field => productObject[field])
-    const sql = `INSERT INTO products (${productFields.join(', ')}) VALUES (${productFields.map(_ => '?').join(',')})`
-    db.run(sql, data, function (err) {
-      if (err) {
-        console.log(err.message)
-      } else {
-        console.log(`insertProductToDB :: Rows affected: ${this.changes}`)
-      }
-      db.close()
+  const db = new sqlite3.Database(DBPath)
+  const getFieldsSqlQuery = 'SELECT DISTINCT name FROM pragma_table_info(\'products\') AS productsTblInfo;'
+  let productFields
+  db.all(getFieldsSqlQuery, [], function (err, rows) {
+    if (err) {
+      throw err
+    } else {
+      productFields = rows.map(row => row.name)
+    }
+    api.getProduct(brand, productCode).then((productObject) => {
+      const productData = productFields.map(field => productObject[field])
+      const insertProductSql = `INSERT INTO products (${productFields.join(', ')}) VALUES (${productFields.map(_ => '?').join(',')})`
+      db.run(insertProductSql, productData, function (err) {
+        if (err) {
+          console.log(err.message)
+        } else {
+          console.log(`insertProductToDB :: Rows affected: ${this.changes}`)
+        }
+        db.close()
+      })
     })
   })
 }
